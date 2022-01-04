68cb75e82629322500cc2c8e710346acc116762d
Nicolas Duflot <nduflot@jehann.fr>
2022-01-04 16:57:58 +0100

    JS dom intro pt 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+// création d'une petite bobliothèque de fonctions 
+// a utiliser sur tout le site
+
+// vérifier le chargement de la page DOMContentLoaded
+function loaded(callable){
+    window.addEventListener('DOMContentLoaded', callable);
+}
+
+// raccourcis du querySelector
+function s(selector){
+    return document.querySelector(selector);
+}
+
+// raccourcis du querySelectorAll
+function sAll(selector){
+    return document.querySelectorAll(selector);
+}
 
 // fonction de création et suppression de cookie
 // par défaut, dans durée en jour du cookie, le cookie appelé sera détruit
